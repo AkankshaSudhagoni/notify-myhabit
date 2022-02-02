85d3e17437c442a01fd4b46b9eebf55cedf2ee56
Rohit Reddy Chandupatla <77590158+RohitChandupatla@users.noreply.github.com>
2022-02-01 20:53:04 -0600

    added DBHandler

---
+++ DBHandler/DBHandler.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace HabitApp.DBHandler
+{
+    public class DBHandler
+    {
+        protected IMongoCollection<BsonDocument> collection = null;
+
+
+        
+        public DBHandler(string collectionName)
+        {
+            this.collection = getMongoCollectionByName(collectionName);
+        }
+
+        //get the collection by passing the collection name
+        public IMongoCollection<BsonDocument> getMongoCollectionByName(String collectionName)
+        {
+            DatabaseUtil dbUtil = new DatabaseUtil();
+            return dbUtil.getCollection(collectionName);
+        }
+
+    }
+}
