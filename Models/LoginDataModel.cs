ef8797596e7813318a78d51bbfb56eef3050b599
Akanksha Sudhagoni <77809588+AkankshaSudhagoni@users.noreply.github.com>
2022-02-01 21:40:57 -0600

    added login data model

---
+++ Models/LoginDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HabitApp.Models
+{
+    public class LoginDataModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
