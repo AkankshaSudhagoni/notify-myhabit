9b0f152acb4eec15332a7f5c5414302b326b3421
Rohit Reddy Chandupatla <77590158+RohitChandupatla@users.noreply.github.com>
2022-02-06 18:59:22 -0600

    added update habit model

---
+++ Models/UpdateHabitModel.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HabitApp.Models
+{
+    public class UpdateHabitModel
+    {
+
+        [BsonId]
+        public string habitId { get; set; }
+        public string habitTitle { get; set; }
+        public string habitDescription { get; set; }
+        public string habitImage { get; set; }
+        public string habitVideoUrl { get; set; }
+        public string habitAudio { get; set; }
+        public bool habitStatus { get; set; }
+        public int habitPoints { get; set; }
+        public string userId { get; set; }
+    }
+}
