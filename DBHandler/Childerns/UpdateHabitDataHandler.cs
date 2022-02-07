ac4e4c14bcd73fa93b2a654e005e502054d523fc
Rohit Reddy Chandupatla <77809588+AkankshaSudhagoni@users.noreply.github.com>
2022-02-06 19:06:38 -0600

    added update habit data handler

---
+++ DBHandler/Childerns/UpdateHabitDataHandler.cs
@@ -0,0 +1,54 @@
+﻿using HabitApp.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HabitApp.DBHandler.Childerns
+{
+    public class UpdateHabitDataHandler : DBHandler
+    {
+        public UpdateHabitDataHandler(string collectionName) : base(collectionName)
+        {
+
+        }
+
+        public Task<bool> UpdateHabit(HabitModel habit)
+        {
+            bool status = false;
+
+            //create a custom bson to pass data to update
+
+            BsonDocument bd = new BsonDocument();
+            bd.Add("habitTitle",habit.habitTitle);
+            bd.Add("habitDescription",habit.habitDescription);
+            bd.Add("habitImage",habit.habitImage);
+            bd.Add("habitVideoUrl",habit.habitVideoUrl);
+            bd.Add("habitAudio",habit.habitAudio);
+            bd.Add("habitStatus",habit.habitStatus);
+            bd.Add("habitPoints",habit.habitPoints);
+            bd.Add("userId",habit.userId);
+
+
+            //update
+            Task updateHabitTask = collection.ReplaceOneAsync("{ _id: ObjectId('"+habit.habitId.ToString()+"')  }", bd);
+            
+            updateHabitTask.Wait();
+
+            if (updateHabitTask.IsCompleted)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            return Task.FromResult(status);
+        }
+
+
+    }
+}
