b7acdb5666f7a70d73520841daf95ec41350857d
Rohit Reddy Chandupatla <77590158+RohitChandupatla@users.noreply.github.com>
2022-02-06 19:20:54 -0600

    added reset password data model

---
+++ Models/ResetPasswordDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HabitApp.Models
+{
+    public class ResetPasswordDataModel
+    {
+        public String code { get; set; }
+        public String newPassword { get; set; }
+        public String confirmPassword { get; set; }
+
+    }
+}
