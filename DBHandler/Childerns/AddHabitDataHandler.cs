8693fa0d658d9b4b665a6530aa829f918b2b3021
Rohit Reddy Chandupatla <77590158+RohitChandupatla@users.noreply.github.com>
2022-02-06 18:55:22 -0600

    added habit data handler

---
+++ DBHandler/Childerns/AddHabitDataHandler.cs
@@ -0,0 +1,37 @@
+﻿using HabitApp.Models;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HabitApp.DBHandler.Childerns
+{
+    public class AddHabitDataHandler : DBHandler
+    {
+        public AddHabitDataHandler(String collectionName) : base(collectionName)
+        {
+
+        }
+
+        public Task<bool> AddHabit(HabitModel habit)
+        {
+            bool status = false;
+
+            Task addHabitTask = collection.InsertOneAsync(habit.ToBsonDocument());
+
+            addHabitTask.Wait();
+
+            if (addHabitTask.IsCompleted)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            return Task.FromResult(status);
+        }
+    }
+}
